2d9b14b72c50fafec4e62e888a687c10f3c47ea5
ALUNO <ALUNO@senaiescola.fsa>
2024-11-05 11:27:50 -0300

    Quiz Tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,86 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Quiz',
+      title: 'Quiz do Universo Marvel',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.red,
       ),
-      home: const QuizScreen(title: 'Quiz App'),
+      home: const TelaInicial(),
+    );
+  }
+}
+
+class TelaInicial extends StatelessWidget {
+  const TelaInicial({super.key});
+
+  @override // construção da tela
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Universo Marvel',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.redAccent,
+                Colors.deepOrange,
+                Colors.black,
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'lib/assets/marvel.jpg',
+              width: 500,
+              height: 500,
+            ),
+            const SizedBox(height: 20),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Bem vindo ao Quiz do Universo Marvel!',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  // botão de reiniciar o quiz
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const QuizScreen(title: 'Quiz'),
+                      ),
+                    );
+                  },
+                  child: const Text('Iniciar Quiz'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
@@ -29,15 +94,15 @@ class QuizScreen extends StatefulWidget {
   _QuizScreenState createState() => _QuizScreenState();
 }
 
-//Padding
-
 class _QuizScreenState extends State<QuizScreen> {
   int _questionIndex = 0;
   int _totalScore = 0;
 
   final List<Map<String, Object>> _questions = [
     {
-      'questionText': 'Qual é a capital da França?', // 1
+      'questionText': 'Qual é a capital da França?',
+      'imageUrl':
+          'https://static.vecteezy.com/ti/vetor-gratis/t2/15451717-bandeira-da-franca-bandeira-do-3d-vetor.jpg',
       'answers': [
         {'text': 'Paris', 'score': 1},
         {'text': 'Londres', 'score': 0},
@@ -45,90 +110,8 @@ class _QuizScreenState extends State<QuizScreen> {
         {'text': 'Berlim', 'score': 0},
       ],
     },
-    {
-      'questionText': 'Qual é o maior planeta do Sistema Solar?', // 2
-      'answers': [
-        {'text': 'Terra', 'score': 0},
-        {'text': 'Júpiter', 'score': 1},
-        {'text': 'Marte', 'score': 0},
-        {'text': 'Saturno', 'score': 0},
-      ],
-    },
-    {
-      'questionText': 'Quem pintou a Mona Lisa?', // 3
-      'answers': [
-        {'text': 'Vincent van Gogh', 'score': 0},
-        {'text': 'Pablo Picasso', 'score': 0},
-        {'text': 'Leonardo da Vinci', 'score': 1},
-        {'text': 'Claude Monet', 'score': 0},
-      ],
-    },
-    {
-      'questionText': 'Qual é o animal terrestre mais rápido do mundo?', // 4
-      'answers': [
-        {'text': 'Elefante', 'score': 0},
-        {'text': 'Guepardo', 'score': 1},
-        {'text': 'Leão', 'score': 0},
-        {'text': 'Cavalo', 'score': 0},
-      ],
-    },
-    {
-      'questionText': 'Qual é a fórmula química da água?', // 5
-      'answers': [
-        {'text': 'H2O', 'score': 1},
-        {'text': 'O2H', 'score': 0},
-        {'text': 'CO2', 'score': 0},
-        {'text': 'HO2', 'score': 0},
-      ],
-    },
-    {
-      'questionText': 'Em que ano o homem pisou na Lua pela primeira vez?', // 6
-      'answers': [
-        {'text': '1965', 'score': 0},
-        {'text': '1969', 'score': 1},
-        {'text': '1975', 'score': 0},
-        {'text': '1980', 'score': 0},
-      ],
-    },
-    {
-      'questionText': 'Quantos continentes existem na Terra?', // 7
-      'answers': [
-        {'text': '5', 'score': 0},
-        {'text': '6', 'score': 0},
-        {'text': '7', 'score': 1},
-        {'text': '8', 'score': 0},
-      ],
-    },
-    {
-      'questionText': 'Qual é o elemento químico representado pelo símbolo “O”?', // 8
-      'answers': [
-        {'text': 'Ouro', 'score': 0},
-        {'text': 'Oxigênio', 'score': 1},
-        {'text': 'Ósmio', 'score': 0},
-        {'text': 'Organônio', 'score': 0},
-      ],
-    },
-    {
-      'questionText': 'Qual é o país mais populoso do mundo?', // 9
-      'answers': [
-        {'text': 'Índia', 'score': 0},
-        {'text': 'Estados Unidos', 'score': 0},
-        {'text': 'China', 'score': 1},
-        {'text': 'Brasil', 'score': 0},
-      ],
-    },
-    {
-      'questionText': 'Quantos graus tem um ângulo reto?', // 10
-      'answers': [
-        {'text': '45', 'score': 0},
-        {'text': '90', 'score': 1},
-        {'text': '180', 'score': 0},
-        {'text': '360', 'score': 0},
-      ],
-    },
+    // Adicione outras perguntas conforme necessário
   ];
-
-  
 
   void _answerQuestion(int score) {
     setState(() {
@@ -142,27 +125,41 @@ class _QuizScreenState extends State<QuizScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: _questionIndex < _questions.length
-          ? Column(
-              children: [ 
-                Text(_questions[_questionIndex]['questionText'] as String),
-                ...(_questions[_questionIndex]['answers']
-                        as List<Map<String, Object>>)
-                    .map((answer) {
-                  return ElevatedButton(
-                    onPressed: () => _answerQuestion(answer['score'] as int),
-                    child: Text(answer['text'] as String),
-                  );
-                }).toList()
-              ],
-            )
-          : Center(
-              child: Text(
-                'Parabéns! Sua pontuação foi: $_totalScore',
-                style: const TextStyle(fontSize: 24),
+      body: Center(
+        child: _questionIndex < _questions.length
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.network(
+                    _questions[_questionIndex]['imageUrl'] as String,
+                    height: 200,
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    _questions[_questionIndex]['questionText'] as String,
+                    style: const TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  ...(_questions[_questionIndex]['answers']
+                          as List<Map<String, Object>>)
+                      .map((answer) {
+                    return ElevatedButton(
+                      onPressed: () => _answerQuestion(answer['score'] as int),
+                      child: Text(answer['text'] as String),
+                    );
+                  }).toList(),
+                ],
+              )
+            : Center(
+                child: Text(
+                  'Parabéns! Sua pontuação foi: $_totalScore',
+                  style: const TextStyle(fontSize: 24),
+                ),
               ),
-            ),
+      ),
     );
   }
 }
